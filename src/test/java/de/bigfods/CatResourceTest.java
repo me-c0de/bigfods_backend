cc3252c1b1d08e190606f7b12074bd876ef25ab9
Christopher Meyer <christopher_meyer@outlook.de>
2022-05-17 13:01:53 +0200

    core: update gradle 6.8 -> 7.4, java 11 -> 18

--- src/test/java/de/bigfods/CatResourceTest.java
+++ src/test/java/de/bigfods/CatResourceTest.java
@@ -9,7 +9,6 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class CatResourceTest {
 
-    @Test
     public void testHelloEndpoint() {
         given()
           .when().get("/hello")
